4bd889fe03e62006883ebbcaf5cd0697f375e00b
zeehou <wisehou@outlook.com>
2021-08-02 13:59:16 +0800

    111

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Counter-Strike: Global Offensive</h1>
+      <!-- <h1>Config</h1> -->
+      
+      <div class="test">
+        <n-card
+          id="config"
+          title="Config"
+          hoverable
+          :segmented="{
+            content: 'hard',
+            footer: 'soft',
+          }"
+        >
+          <div class="c1">
+            整合版(网吧)：<n-button tag="a" href="cfg/wb.cfg">wb.cfg</n-button>
+          </div>
+
+          <div>
+            独立版(家里)：<n-button
+              class="btnconfig"
+              tag="a"
+              href="cfg/config.cfg"
+              >config</n-button
+            >
+            <n-button tag="a" href="cfg/autoexec.cfg">autoexec</n-button>
+          </div>
+        </n-card>
+        <n-card
+          id="config"
+          title="Test"
+          hoverable
+          :segmented="{
+            content: 'hard',
+            footer: 'soft',
+          }"
+        >
+          <div class="c1">
+            媒体测试：<n-button tag="a" href="https://www.onlinemictest.com/zh/"
+              >Mic Test</n-button
+            >
+          </div>
+
+          <div>
+            鼠标测试：<n-button
+              class="btnconfig"
+              tag="a"
+              href="tool/MouseRateChecker.rar"
+              >RateTest</n-button
+            >
+            <n-button tag="a" href="tool/MouseTest.zip">MouseTest</n-button>
+          </div>
+        </n-card>
+      </div>
+    </header>
+
+    <h2>Pro Config：</h2>
+
+    <crosshair />
+
+    <n-back-top :right="40" />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import crosshair from "./components/crosshair.vue";
+import { NButton, NBackTop, NCard } from "naive-ui";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+
+    crosshair,
+    NButton,
+    NBackTop,
+    NCard,
+  },
+};
+</script>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Oswald:300,400,700);
+@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic);
+
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  -webkit-font-smoothing: antialiased;
+  font-family: "Oswald", sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  margin: 20px;
+}
+body {
+  background-color: rgba(136, 136, 136, 0.041);
+}
+
+.c1 {
+  margin-bottom: 5px;
+}
+.btnconfig {
+  margin-right: 5px;
+}
+.n-button {
+  width: 60px;
+}
+#config {
+  width: 400px;
+  margin: 5px;
+  /* margin: 0 auto; */
+}
+.test {
+  display: flex;
+  /* flex-direction: column; */
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/crosshair.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="cross">
+    <n-card
+      class="crossbox2"
+      title="S1mple"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshairgap -3; cl_crosshairsize 1; cl_crosshairalpha 250;
+      cl_crosshaircolor 4; cl_crosshairdot 1; cl_crosshairstyle 4;
+      cl_crosshairusealpha 1; cl_crosshairthickness 0; cl_crosshair_sniper_width
+      1; cl_crosshair_drawoutline 0; cl_crosshaircolor_r 255;
+      cl_crosshaircolor_g 255; cl_crosshaircolor_b 255;
+      <template #footer
+        ><n-button href='cfg/s1mple.zip' ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+
+    <n-card
+      class="crossbox2"
+      title="B1t"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshair_drawoutline 0; cl_crosshair_sniper_width 1; cl_crosshairalpha
+      255; cl_crosshaircolor 4; cl_crosshaircolor_b 144; cl_crosshaircolor_g
+      238; cl_crosshaircolor_r 0; cl_crosshairdot 0; cl_crosshairgap -3;
+      cl_crosshairsize 2; cl_crosshairstyle 4; cl_crosshairthickness 0;
+      cl_crosshairusealpha 1;
+
+      <template #footer
+        ><n-button ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+
+    <n-card
+      class="crossbox2"
+      title="NiKo"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshairalpha 255; cl_crosshaircolor 5; cl_crosshairdot 0;
+      cl_crosshairgap -4; cl_crosshairsize 2; cl_crosshairstyle 4;
+      cl_crosshairusealpha 1; cl_crosshairthickness 1; cl_crosshair_drawoutline
+      0; cl_crosshair_sniper_width 1; cl_crosshaircolor_r 255;
+      cl_crosshaircolor_g 255; cl_crosshaircolor_b 250;
+      <template #footer
+        ><n-button ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+
+    <n-card
+      class="crossbox2"
+      title="arT"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshairalpha 255; cl_crosshaircolor 1; cl_crosshairdot 0;
+      cl_crosshairgap -3; cl_crosshairsize 2; cl_crosshairstyle 4;
+      cl_crosshairusealpha 1; cl_crosshairthickness 0; cl_crosshair_drawoutline
+      0;
+
+      <template #footer
+        ><n-button ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+
+    <n-card
+      class="crossbox2"
+      title="ZywOo"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshairsize 2; cl_crosshairstyle 4; cl_crosshairgap -3;
+      cl_crosshairdot 0; cl_crosshairthickness 0; cl_crosshair_drawoutline 0;
+      cl_crosshair_outlinethickness 0.5; cl_crosshaircolor 1;
+      cl_crosshaircolor_r 255; cl_crosshaircolor_g 255; cl_crosshaircolor_b 255;
+      cl_crosshair_sniper_width 1;
+
+      <template #footer
+        ><n-button ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+
+    <n-card
+      class="crossbox2"
+      title="ropz"
+      :segmented="{
+        content: 'hard',
+        footer: 'soft',
+      }"
+    >
+      <template #header-extra></template>
+      cl_crosshairalpha 250; cl_crosshaircolor 1; cl_crosshairdot 0;
+      cl_crosshairgap -3; cl_crosshairsize 2; cl_crosshairstyle 4;
+      cl_crosshairusealpha 1; cl_crosshairthickness 0; cl_crosshair_drawoutline
+      0;
+      <template #footer
+        ><n-button ghost color="#ff69b4">Config</n-button></template
+      >
+    </n-card>
+  </div>
+</template>
+
+<script>
+import { NButton, NCard } from "naive-ui";
+
+export default {
+  name: "crosshair",
+  components: {
+    NButton,
+    NCard,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* n-scrollbar{
+  overflow: scroll;
+  height: 100%;
+} */
+.crossbox {
+  width: 260px;
+}
+.crossbox2 {
+  width: 200px;
+  margin: 5px;
+  line-height: 130%;
+  /* height: 300px; */
+}
+
+.light-green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.12);
+  /* width: 200px; */
+}
+.green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.24);
+  /* width: 200px; */
+}
+/* n-gi div{
+  width: 200px;
+} */
+/* .gridbox{
+  display: grid;
+  grid-template-columns: repeat(2, 500px 20px 80px);
+} */
+.cross {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
